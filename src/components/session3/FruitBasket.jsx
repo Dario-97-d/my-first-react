445b4758083a6dade14a2d2b5a209ab83ee6fdf2
Dario-97-d <dariodias97@gmail.com>
2026-03-09 19:53:07 +0000

    Improve feature Fruit Basket
    - Removed option to clear fruits.
    - Made new fruit kind necessarily be different from the previous one.
    - Added background-color and hover effect to fruit buttons to suggest they are clickable.
    - Added background-color to Add and Eat buttons.
    - Made button Eat turn disabled when there aren't fruits.

--- src/components/session3/FruitBasket.jsx
+++ src/components/session3/FruitBasket.jsx
@@ -24,32 +24,68 @@ export default function FruitBasket()
         setFruits(fruits.slice(0, -1))
     }
 
-    const clearFruits = () => {
-        setFruits([])
+    const switchFruit = (id) => {
+        setFruits(fruits.map(f => {
+            if (f.id !== id) return f
+
+            let newFruitKind = randomFruitKind()
+            while (newFruitKind === f.kind) {
+              newFruitKind = randomFruitKind()
+            }
+
+            return { ...f, kind: newFruitKind }
+        }))
     }
 
-    const switchFruit = (id) => {
-        setFruits(fruits.map(f => 
-            f.id === id ? { ...f, kind: randomFruitKind() } : f
-        ))
+    const areThereFruits = fruits.length > 0
+
+    const buttonStyles = {
+        fruit: {
+            backgroundColor: 'rgba(0,127,255,0.75)',
+            transition: 'background-color 1s ease'
+        },
+        add: {
+            backgroundColor: 'green',
+            color: 'white',
+            transition: 'background-color 0.3s ease-in-out'
+        },
+        eat: {
+            backgroundColor: areThereFruits ? 'red' : 'gray',
+            color: 'white',
+            transition: 'background-color 0.3s ease-in-out'
+        },
+    }
+
+    const fruitButtonHover = (btnId) => {
+      document.getElementById(btnId).style.backgroundColor = 'rgba(0,127,255,0.5)'
+    }
+
+    const fruitButtonUnhover = (btnId) => {
+      document.getElementById(btnId).style.backgroundColor = 'rgba(0,127,255,0.75)'
     }
 
     return (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '2rem' }}>
-            <div style={{ fontSize: '1.5em', fontWeight: 'bold' }}>Fruit Basket</div>
+            <div style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>Random Fruits</div>
 
             <div style={{ margin: '1rem', width: '20rem', flexWrap: 'wrap', display: 'flex', gap: '1em', justifyContent: 'center' }}>
                 {fruits.map(f => (
-                    <button key={f.id} onClick={() => switchFruit(f.id)}>
+                    <button
+                        key={f.id}
+                        id={f.id}
+                        style={buttonStyles.fruit}
+                        onClick={() => switchFruit(f.id)}
+                        onMouseOver={() => fruitButtonHover(f.id)}
+                        onMouseLeave={() => fruitButtonUnhover(f.id)}
+                    >
                         {f.kind}
                     </button>
                 ))}
             </div>
 
             <div style={{ display: 'flex', gap: '1em', justifyContent: 'center' }}>
-                <button onClick={addFruit}>Add</button>
-                <button onClick={eatFruit}>Eat</button>
-                <button onClick={clearFruits}>Clear</button>
+                <button onClick={addFruit} style={buttonStyles.add}>Add</button>
+                <button onClick={eatFruit} style={buttonStyles.eat} disabled={!areThereFruits}>Eat</button>
             </div>
         </div>
     )
