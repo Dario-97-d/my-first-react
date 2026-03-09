a4db980fb6cf3f62e007493ebfbd9f495005129d
Dario-97-d <dariodias97@gmail.com>
2026-03-09 20:01:16 +0000

    Make Numbers Table showcase Flexboxing responsiveness
    - Added horizontal resizing for user interaction, so that a user can play with its Flexbox responsiveness.

--- src/components/IronHack/NumbersTable.jsx
+++ src/components/IronHack/NumbersTable.jsx
@@ -1,18 +1,61 @@
+import { useEffect, useRef } from 'react'
 import styles from './NumbersTable.module.css'
 
 function NumbersTable(props)
 {
     const { limit } = props
 
+    const wrapperRef = useRef(null)
+    const handleRef = useRef(null)
+
     const numbers = Array.from({ length: limit }, (e, i) => i + 1)
 
-    return (
-        <div className={ styles['div-table'] }>
-            
-            {numbers.map(n => <div key={n} className={ styles['div-number'] }>{n}</div>)}
+    useEffect(() => {
+        const wrapper = wrapperRef.current
+        const handle = handleRef.current
 
+        let startX
+        let startWidth
+
+        const onMouseMove = (e) => {
+            const dx = e.clientX - startX
+            wrapper.style.width = `${startWidth + dx}px`
+        }
+
+        const onMouseUp = () => {
+            window.removeEventListener('mousemove', onMouseMove)
+            window.removeEventListener('mouseup', onMouseUp)
+        }
+
+        const onMouseDown = (e) => {
+            startX = e.clientX
+            startWidth = wrapper.offsetWidth
+
+            window.addEventListener('mousemove', onMouseMove)
+            window.addEventListener('mouseup', onMouseUp)
+        }
+
+        handle.addEventListener('mousedown', onMouseDown)
+
+        return () => {
+            handle.removeEventListener('mousedown', onMouseDown)
+        }
+    }, [])
+
+    return (<>
+        <h3>Flexboxing</h3>
+
+        <div ref={wrapperRef} className={ styles['wrapper'] }>
+            <div className={ styles['div-table'] }>
+
+                {numbers.map(n => <div key={n} className={ styles['div-number'] }>{n}</div>)}
+
+            </div>
+
+            <div ref={handleRef} className={ styles['pan-for-resize'] }>
+            </div>
         </div>
-    )
+    </>)
 }
 
 export default NumbersTable
